6f40865d5d53bb7af213c8f7c78930ddaff3e42c
Huang <1579515851@qq.com>
2023-12-05 21:09:01 +0800

    添加了remote.cpp/.h

---
+++ remote/remote.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 15795 on 2023/12/5.
+//
+
+#include "remote.h"
---
+++ remote/remote.h
@@ -0,0 +1,8 @@
+//
+// Created by 15795 on 2023/12/5.
+//
+
+#ifndef EC_HOMEWORK_REMOTE_H
+#define EC_HOMEWORK_REMOTE_H
+
+#endif //EC_HOMEWORK_REMOTE_H
