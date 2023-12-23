0fbe8a07148d6e2c931f65773668f2274d326e6a
Huang <1579515851@qq.com>
2023-12-23 22:31:35 +0800

    添加了PID类定义了PID类的构造函数

--- PID/PID.cpp
+++ PID/PID.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "PID.h"
+
+PID::PID(float kp, float ki, float kd, float i_max, float out_max)
+{
+    kp_ = kp;
+    kd_ = kd;
+    ki_ = ki;
+    i_max_ = i_max;
+    out_max_ = out_max;
+    err_ = 0;
+    last_err_ = 0;
+    err_sum_ = 0;
+}
