62d0e85c295d6665a90dfc857f805b5f647cba21
Huang <1579515851@qq.com>
2023-12-23 22:53:58 +0800

    添加了math限幅函数和取符号函数

--- Math/math.cpp
+++ Math/math.cpp
@@ -3,3 +3,40 @@
 //
 
 #include "math.h"
+// Value limitation
+// 限幅
+float math::limit(float val, const float& min, const float& max) {
+    if (min > max)
+        return val;
+    else if (val < min)
+        val = min;
+    else if (val > max)
+        val = max;
+    return val;
+}
+
+// Value limitation
+// 限幅
+float math::limitMin(float val, const float& min) {
+    if (val < min)
+        val = min;
+    return val;
+}
+
+// Value limitation
+// 限幅
+float math::limitMax(float val, const float& max) {
+    if (val > max)
+        val = max;
+    return val;
+}
+
+// Get sign of value
+// 符号
+float math::sign(const float& val) {
+    if (val > 0)
+        return 1;
+    else if (val < 0)
+        return -1;
+    return 0;
+}
--- Math/math.h
+++ Math/math.h
@@ -4,5 +4,18 @@
 
 #ifndef EC_HOMEWORK_MATH_H
 #define EC_HOMEWORK_MATH_H
+#include <stdint.h>
+
+namespace math {
+
+// Value limitation 限幅
+    float limit(float val, const float &min, const float &max);
+
+    float limitMin(float val, const float &min);
+
+    float limitMax(float val, const float &max);
+
+    float sign(const float &val);
+}
 
 #endif //EC_HOMEWORK_MATH_H
