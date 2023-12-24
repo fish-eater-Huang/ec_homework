1371a281cddd476364453db972c2e1b5fbd43608
Huang <1579515851@qq.com>
2023-12-24 11:07:56 +0800

    添加了Motor类的reset() 函数

--- Motor/motor.cpp
+++ Motor/motor.cpp
@@ -14,3 +14,22 @@ Motor::Motor(const Type_e& type, const float& ratio, const ControlMethod_e& meth
     ppid_ = ppid;
     spid_ = spid;
 }
+
+void Motor::Reset()
+{
+    motor_data_.angle = 0;
+    motor_data_.rotate_speed = 0;
+
+    target_angle = 0;
+
+    target_speed = 0;
+
+    motor_data_.angle = 0;
+    motor_data_.current = 0;
+    motor_data_.ecd_angle = 0;
+    motor_data_.rotate_speed = 0;
+    motor_data_.temp = 0;
+
+    ppid_.reset();
+    spid_.reset();
+}
