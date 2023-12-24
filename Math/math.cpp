91b91db791e16eda57d5da79d84bba3dde6dc9e8
Huang <1579515851@qq.com>
2023-12-24 17:01:49 +0800

    加入了更多函数

--- Math/math.cpp
+++ Math/math.cpp
@@ -57,6 +57,61 @@ float math::loopLimit(float val, const float& min, const float& max) {
     return val;
 }
 
+// Dead band
+// 死区
+float math::deadBand(float val, const float& min, const float& max) {
+    if (val < max && val > min) {
+        val = 0;
+    }
+    return val;
+}
+
+// Angle unit conversion
+// 角度单位换算
+float math::rad2deg(const float& angle) {
+    return angle * 57.2957795f;
+}
+
+float math::deg2rad(const float& angle) {
+    return angle * 0.01745329f;
+}
+
+float math::ecd2deg(const float& ecd, const float& ecd_range) {
+    return ecd * 360.f / ecd_range;
+}
+
+float math::ecd2rad(const float& ecd, const float& ecd_range) {
+    return ecd * 2.f * PI / ecd_range;
+}
+
+// Rotational speed unit conversion(rpm-revolution/min, radps-rad/s, dps-°/s)
+// 角速度单位换算(rpm-转/min, radps-rad/s, dps-°/s)
+float math::rpm2radps(const float& w) {
+    return w * 0.10471976f;
+}
+
+float math::radps2rpm(const float& w) {
+    return w * 9.54929658f;
+}
+
+float math::radps2dps(const float& w) {
+    return w * 57.2957795f;
+}
+
+float math::dps2radps(const float& w) {
+    return w * 0.01745329f;
+}
+
+float math::rpm2dps(const float& w) {
+    return w * 6.f;
+}
+
+float math::dps2rpm(const float& w) {
+    return w * 0.16666667f;
+}
+
+// Angle normalization(rad->[-PI,PI], deg->[-180,180])
+// 角度规范化(rad->[-PI,PI], deg->[-180,180])
 float math::radNormalizePI(const float& angle) {
     return math::loopLimit(angle, -PI, PI);
 }
